b909a1e2bcd9f09e654294bbe93d97b4c2d01cb4
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-10-30 06:07:31 +0530

    Kandane's Algorithm - Brute Force and Kandane's Algorithm solution added

--- src/test/java/kandaneAlgorithm/FindMaxSumSubArray.java
+++ src/test/java/kandaneAlgorithm/FindMaxSumSubArray.java
@@ -65,15 +65,15 @@ public class FindMaxSumSubArray {
 		//Positive Test Data
 		int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
 		int sum = 6;
-		Assert.assertEquals(findMaxSumSubArray(nums), sum);
+		Assert.assertEquals(findMaxSumSubArray_bruteForce(nums), sum);
 	}
 	
 	@Test
 	public void example2() {
 		//Edge Case Test Data
-		int[] nums = {0,-3,5};
-		int sum = 5;
-		Assert.assertEquals(findMaxSumSubArray(nums), sum);
+		int[] nums = {0,3,5,0};
+		int sum = 8;
+		Assert.assertEquals(findMaxSumSubArray_kandane(nums), sum);
 	}
 	
 	
@@ -81,21 +81,60 @@ public class FindMaxSumSubArray {
 	@Test
 	public void example3() {
 		//Negative Test Data
-		
+		int[] nums = {-1,-3,-5};
+		int sum = -1;
+		Assert.assertEquals(findMaxSumSubArray_kandane(nums), sum);
 	}
 	
 	/*
 	 * --- Pseudo Code ---
 	 * 
+	 * -- Brute Force --
+	 * 
+	 * 1. Create variables maxSum=Integer.MIN_VALUE and sum=0
+	 * 2. Traverse through the given array using two loops. i=0 and j=i
+	 * 3. Initiate sum=0 for every i value
+	 * 4. Add sum with current value and find maxSum after every sum
+	 * 5. Return maxSum
+	 * 
+	 * Kandane's algorithm : When sum less than 0 is found, reset it to 0
+	 * 
+	 * 1. Create pointers maxSum=0,sum=0,negMax=Integer.MIN_VALUE and a boolean flag to check if nonZero is available
+	 * 2. Traverse through the given array
+	 * 3. add current value to sum
+	 * 4. If sum < 0, check negMax and reset sum to 0
+	 * 5. Calculate the max between maxSum and sum
+	 * 6. If maxSum greater than 0 and nonZero available flag is false, make it as true
+	 * 7. If nonZero flag is true, return maxSum and if nonZero flag is false, return negMax
+	 * 
+	 * 
 	 */	
 	
-	private int findMaxSumSubArray(int[] nums) {
-		int maxSum=0,sum=0;
+	private int findMaxSumSubArray_bruteForce(int[] nums) {
+		int maxSum=Integer.MIN_VALUE,sum=0;
 		for(int i=0;i<nums.length;i++) {
-			sum += nums[i];
-			if(sum<0) sum=0;
-			maxSum = Math.max(sum, maxSum);
+			sum=0;
+			for(int j=i;j<nums.length;j++) {
+				sum += nums[j];
+				maxSum = Math.max(sum, maxSum);
+			}
 		}
 		return maxSum;
+	}
+	
+	private int findMaxSumSubArray_kandane(int[] nums) {
+		int maxSum=0,sum=0;
+		int negMax = Integer.MIN_VALUE;
+		boolean isNonZeroAvailable = false;
+		for(int i=0;i<nums.length;i++) {
+			sum += nums[i];
+			if(sum<0) {
+				if(negMax < sum) negMax = sum;
+				sum=0;
+			}
+			maxSum = Math.max(sum, maxSum);
+			if(maxSum > 0 && !isNonZeroAvailable) isNonZeroAvailable = true;
+		}
+		return isNonZeroAvailable ? maxSum : negMax;
 	}
 }
