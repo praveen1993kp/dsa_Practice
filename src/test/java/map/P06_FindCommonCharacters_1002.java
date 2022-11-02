d1a17c4e88235f1883950ee560a50e442042226c
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-11-02 10:25:11 +0530

    Leetcode #1002 - Find Common Characters - Better Ascii Approach Added

--- src/test/java/map/P06_FindCommonCharacters_1002.java
+++ src/test/java/map/P06_FindCommonCharacters_1002.java
@@ -69,7 +69,7 @@ public class P06_FindCommonCharacters_1002 {
 		//Positive Test Data
 		String[] words = {"bella","label","roller"};
 		List<String> output = Arrays.asList("e","l","l");
-		Assert.assertEquals(commoncharacters(words), output);
+		Assert.assertEquals(commoncharacters_betterApproach(words), output);
 	}
 	
 	@Test
@@ -77,15 +77,15 @@ public class P06_FindCommonCharacters_1002 {
 		//Edge Case Test Data
 		String[] words = {"car","carrac","a"};
 		List<String> output = Arrays.asList("a");
-		Assert.assertEquals(commoncharacters(words), output);
+		Assert.assertEquals(commoncharacters_betterApproach(words), output);
 	}
 	
 	@Test
 	public void negative() {
 		//Negative Test Data
 		String[] words = {"abc","def","ghi"};
-		List<String> output = Arrays.asList("");
-		Assert.assertEquals(commoncharacters(words), output);
+		List<String> output = new ArrayList<>();
+		Assert.assertEquals(commoncharacters_betterApproach(words), output);
 	}
 	
 	@Test
@@ -93,7 +93,7 @@ public class P06_FindCommonCharacters_1002 {
 		//Negative Test Data
 		String[] words = {"cool","lock","cook"};
 		List<String> output = Arrays.asList("c","o");
-		Assert.assertEquals(commoncharacters(words), output);
+		Assert.assertEquals(commoncharacters_betterApproach(words), output);
 	}
 	
 	/*
@@ -103,6 +103,19 @@ public class P06_FindCommonCharacters_1002 {
 	 * 2. Create pointers left=0 and right=words[0].length()-1
 	 * 3. Add the values of all the characters available in words[0]
 	 * 4. Traverse from words[1] till length of words. Everytime, reset the values of ascii2 to 0
+	 * 5. For each word, add the characters into ascii2
+	 * 6. Compare ascii1 and ascii2 and add the minimum into ascii1
+	 * 7. Continue till step 4 is true
+	 * 8. Create a list for output
+	 * 9. For each index in ascii1, get the value. while the value>0, add the character into arraylist
+	 * 10. Return the list.
+	 * 
+	 * -- Better Approach --
+	 * 
+	 * 1. Create two ascii arrays of size 26
+	 * 2. Create pointers left=0 and right=words[0].length()-1
+	 * 3. Fill ascii1 with value 101
+	 * 4. Traverse from words[0] till length of words. Everytime, fill ascii2 with 0
 	 * 5. For each word, add the characters into ascii2
 	 * 6. Compare ascii1 and ascii2 and add the minimum into ascii1
 	 * 7. Continue till step 4 is true
@@ -120,6 +133,36 @@ public class P06_FindCommonCharacters_1002 {
             ascii1[words[0].charAt(left++)-'a']++;
             if(left<right) ascii1[words[0].charAt(right--)-'a']++;
         }
+        for(int i=0;i<words.length;i++){
+            left=0;
+            right=words[i].length()-1;
+            Arrays.fill(ascii2,0);
+            while(left<=right){
+                ascii2[words[i].charAt(left++)-'a']++;
+                if(left<right) ascii2[words[i].charAt(right--)-'a']++;
+            }
+            
+            for(int k=0;k<ascii1.length;k++){
+                ascii1[k] = Math.min(ascii1[k],ascii2[k]);
+            }
+        }
+        //System.out.println(Arrays.toString(ascii1));
+        List<String> lst = new ArrayList<>();
+        for(int p=0;p<ascii1.length;p++){
+                int count = ascii1[p];
+                while(count>0){
+                    lst.add((char) (p+'a') + "");
+                    count--;
+                }
+        }
+        return lst;
+	}
+	
+	private List<String> commoncharacters_betterApproach(String[] words) {
+		int[] ascii1 = new int[26];
+        int[] ascii2 = new int[26];
+        int left=0,right=0;
+        Arrays.fill(ascii1, 101); // Since the maximum size given in the range 1<words[i].length<=100
         for(int i=0;i<words.length;i++){
             left=0;
             right=words[i].length()-1;
