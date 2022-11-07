d8894a690743f8c1dfb4a8ba5a988c3d3b2a0419
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-11-07 22:27:29 +0530

    Leetcode link added for Dutch Flag problem

--- src/test/java/threePointers/S02_GetCategory_DutchFlagVariation_Facebook.java
+++ src/test/java/threePointers/S02_GetCategory_DutchFlagVariation_Facebook.java
@@ -10,6 +10,9 @@ public class S02_GetCategory_DutchFlagVariation_Facebook {
 	/*
 	 * PROBLEM STATEMENT 
 	 * 
+	 * 
+	 * https://leetcode.com/discuss/interview-question/algorithms/124623/facebook-onsite-variation-of-dutch-national-flag-problem
+	 * 
 	 * You have an unsorted array of integers and a function
 	string getCategory(integer), which deterministically returns 1 of three possible strings: 
 	"low", "medium", or "high", depending on the input integer. You need to output an array with all the "low" numbers at the bottom, all the "medium" numbers in the middle, and all the "high" numbers at the top. This is basically a partial sort. Within each category, the order of the numbers does not matter.
