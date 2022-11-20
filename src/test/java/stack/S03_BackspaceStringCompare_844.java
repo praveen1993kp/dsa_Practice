4d9897959a932e8ce8450c3057db281594032156
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-11-20 19:40:11 +0530

    Leetcode - 844 - Back Space String Compare - Stack and LinkedList solution added

--- src/test/java/stack/S03_BackspaceStringCompare_844.java
+++ src/test/java/stack/S03_BackspaceStringCompare_844.java
@@ -1,4 +1,4 @@
-package linkedList;
+package stack;
 
 import java.util.LinkedList;
 import java.util.Stack;
@@ -8,7 +8,7 @@ import org.junit.Test;
 import junit.framework.Assert;
 import linkedList.SinglyLinkedList_Implementation.Node;
 
-public class P04_BackspaceStringCompare_844 {
+public class S03_BackspaceStringCompare_844 {
 	
 	@Test
 	public void example1() {
@@ -63,27 +63,18 @@ public class P04_BackspaceStringCompare_844 {
 	 * 4. Otherwise continue to add elements into respective linkedlists
 	 * 5. Compare lists
 	 * 
+	 * -- Stack --
+	 * 
+	 * 1. Create two stacks
+	 * 2. Keep track of current and previous
+	 * 3. If current is #, 
+	 * 		If list is not empty, pop
+	 * 4. Otherwise continue to add elements into respective stacks
+	 * 5. Compare stacks and return the result
+	 * 
 	 * 
 	 * 
 	 */
-
-	public boolean backSpace(String s, String t) {
-		LinkedList<Character> lst1 = loadedLinkedList(s);
-		LinkedList<Character> lst2 = loadedLinkedList(t);		
-		
-		return lst1.equals(lst2);
-	}
-	
-	public LinkedList<Character> loadedLinkedList(String s){
-		LinkedList<Character> lst = new LinkedList<>();
-		for(char c : s.toCharArray()) {
-			if(c=='#') {
-				if(!lst.isEmpty())
-					lst.removeLast();
-			} else lst.add(c);
-		}
-		return lst;
-	}
 	
 	public boolean backSpace_UsingStack(String s, String t) {
 		Stack<Character> stack1 = new Stack<>();
@@ -104,5 +95,25 @@ public class P04_BackspaceStringCompare_844 {
         
         return stack1.equals(stack2);
 	}
+
+	public boolean backSpace(String s, String t) {
+		LinkedList<Character> lst1 = loadedLinkedList(s);
+		LinkedList<Character> lst2 = loadedLinkedList(t);		
+		
+		return lst1.equals(lst2);
+	}
+	
+	public LinkedList<Character> loadedLinkedList(String s){
+		LinkedList<Character> lst = new LinkedList<>();
+		for(char c : s.toCharArray()) {
+			if(c=='#') {
+				if(!lst.isEmpty())
+					lst.removeLast();
+			} else lst.add(c);
+		}
+		return lst;
+	}
+	
+	
 	
 }
