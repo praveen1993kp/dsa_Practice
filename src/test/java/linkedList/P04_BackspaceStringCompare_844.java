18a4177665d86bebe3e07332747a29ab9f0e6dde
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-11-20 17:04:36 +0530

    Leetcode #884 - Stack solution added

--- src/test/java/linkedList/P04_BackspaceStringCompare_844.java
+++ src/test/java/linkedList/P04_BackspaceStringCompare_844.java
@@ -85,4 +85,24 @@ public class P04_BackspaceStringCompare_844 {
 		return lst;
 	}
 	
+	public boolean backSpace_UsingStack(String s, String t) {
+		Stack<Character> stack1 = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='#'){
+                if(!stack1.isEmpty())
+                    stack1.pop();
+            } else stack1.push(s.charAt(i));
+        }
+        
+        for(int i=0;i<t.length();i++){
+            if(t.charAt(i)=='#'){
+                if(!stack2.isEmpty())
+                    stack2.pop();
+            } else stack2.push(t.charAt(i));
+        }
+        
+        return stack1.equals(stack2);
+	}
+	
 }
