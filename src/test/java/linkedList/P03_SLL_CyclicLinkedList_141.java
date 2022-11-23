7ad56089a93563995d2ead3ab10c9736129f459b
Praveen Krishnamoorthy <praveen1993kp@gmail.com>
2022-11-23 20:10:25 +0530

    Leetcode #141 - Cyclic Linked List - Solution Added - @@Execute in Leetcode for executing and seeing the output@@

--- src/test/java/linkedList/P03_SLL_CyclicLinkedList_141.java
+++ src/test/java/linkedList/P03_SLL_CyclicLinkedList_141.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 import junit.framework.Assert;
 import linkedList.SinglyLL_SongPlayList.PlayListNode;
 // For the solution to work, execut in Leetcode
-public class P02_SLL_CyclicLinkedList_141 {
+public class P03_SLL_CyclicLinkedList_141 {
 	Node head;
 	Node tail;
 	int length;
